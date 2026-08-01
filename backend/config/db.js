d923c9d174a5832836330eaafd9e4f047849983f
fathima--shaba <fathimashaba674@gmail.com>
2026-08-01 18:19:54 +0530

    Feat: Add support for custom DB Port and SSL for Cloud Database deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,9 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined
 });
 
 db.connect((err) => {
